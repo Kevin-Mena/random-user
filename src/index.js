c7201b7bb2d1e39efb72e0f72e5ac17b629151f8
Kevin Okoth <kevinokotth04@gmail.com>
2023-04-05 14:31:01 +0300

    Add slice for states

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import App from "./App";
+import store from "./features/store";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
+ReactDOM.render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>,
+  document.getElementById("root")
 );
